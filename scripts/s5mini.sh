12d93b640f37fe5a721e56a2f01f89cd76b86ed6
Vicente Cortes <vicenteicc2008@gmail.com>
2023-08-26 19:04:48 -0400

    add s5 mini duos initial support

---
+++ scripts/s5mini.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# based on the instructions from edk2-platform
+set -e
+. ./scripts/build_common.sh
+# not actually GCC5; it's GCC7 on Ubuntu 18.04.
+GCC5_ARM_PREFIX=arm-linux-gnueabi- build -j$(nproc) -s -n 0 -a ARM -t GCC5 -b DEBUG -p MSM8909Pkg/Devices/s5miniduos.dsc
+./scripts/build_bootshim.sh
+cat BootShim/BootShim.bin workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
+gzip -c < workspace/bootpayload.bin > workspace/zImage
+cat workspace/zImage device_specific/s5mini.dtb > workspace/boot.img
