b984300374cae10cfc3fb11fbbb1c37705520572
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-27 14:30:51 -0300

    update fb address

--- MSM8909Pkg/PrePi/Pi.c
+++ MSM8909Pkg/PrePi/Pi.c
@@ -32,7 +32,7 @@ STATIC VOID UartInit(VOID)
   SerialPortInitialize();
   
   /* Clear screen at new FB address */ 
-  UINT8 *base = (UINT8 *)0x00400000ull;
+  UINT8 *base = (UINT8 *)0x03200000ull;
   for (UINTN i = 0; i < 0x00800000; i++) {
     base[i] = 0;
   }
