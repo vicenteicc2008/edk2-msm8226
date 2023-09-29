eac21b2327d4bdf1dbee7db895359385c71dc958
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-29 15:14:24 -0300

    remove bootshim from build script for now

--- scripts/s5mini.sh
+++ scripts/s5mini.sh
@@ -5,5 +5,5 @@ set -e
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_ARM_PREFIX=arm-linux-gnueabi- build -j$(nproc) -s -n 0 -a ARM -t GCC5 -b DEBUG -p MSM8909Pkg/Devices/s5miniduos.dsc
 ./scripts/build_bootshim.sh
-cat BootShim/BootShim.bin workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
+cat workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
 gzip -c < workspace/bootpayload.bin > workspace/zImage
