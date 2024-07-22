fc412f41a28c2e9c09932116edd6e40b646494da
Vicente Cortes <vicenteicc2008@gmail.com>
2024-07-22 09:11:57 -0400

    remove lk2nd support for s3neo (s3ve3g)

--- scripts/s3neo.sh
+++ scripts/s3neo.sh
@@ -6,5 +6,3 @@ set -e
 GCC5_ARM_PREFIX=arm-linux-gnueabi- build -j$(nproc) -s -n 0 -a ARM -t GCC5 -b DEBUG -p MSM8909Pkg/Devices/s3neo.dsc
 ./scripts/build_bootshim.sh
 cat BootShim/BootShim.bin workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
-gzip -c < workspace/bootpayload.bin > workspace/zImage
-cat workspace/zImage device_specific/s3ve3g.dtb > workspace/boot.img
